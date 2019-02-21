4b2e32208157eb12b05ed18ff19fe9d581ff866a
Panvan K <panvan.k@hashedin.com>
2019-02-21 11:14:59 +0530

    completed post request to add an employee

--- src/main/java/com/hashedin/hu/EmpController.java
+++ src/main/java/com/hashedin/hu/EmpController.java
@@ -1,8 +1,10 @@
 package com.hashedin.hu;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ public class EmpController {
         Employee e2 = new Employee("suman","2019-01-01",10,"MALE");
         employeeService.addEmployee(e2);
     }
-    @RequestMapping("/seeemployee")
+    @RequestMapping("/employee")
     public List<Employee> getEmployees(){
         return employeeService.getAllEmployeees();
     }
@@ -27,6 +29,14 @@ public class EmpController {
             return new ResponseEntity("Employee with id" + id +"not found", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+    }
+    @RequestMapping(value = "/employees/",method = RequestMethod.POST)
+    public ResponseEntity<Void> createEmployee(@RequestBody Employee employee, UriComponentsBuilder ucBuilder){
+        System.out.println("Creating User "+employee.getName());
+        employeeService.addEmployee(employee);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/employees/{id}").buildAndExpand(employee.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 }
 
